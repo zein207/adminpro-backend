db4e105ea1c9f285d9b3a03165a345cfddf8ea23
david <zein207@gmail.com>
2020-12-23 17:56:34 -0600

    Busqueda subida de archivo

---
+++ controllers/medicos.js
@@ -0,0 +1,70 @@
+const { response } = require('express')
+const Medico = require('../models/medico');
+
+const getMedicos = async(req, res = response) => {
+
+    const medicos = await Medico.find()
+                                .populate('usuario', 'nombre img')
+                                .populate('hospital', 'nombre');
+
+    res.json({
+        ok: true,
+        medicos
+    })
+}
+
+const crearMedico = async(req, res = response) => {
+
+    const uid = req.uid;
+    const medico = new Medico({
+        usuario: uid,
+        ...req.body
+    });
+
+
+    try {
+
+        const medicoDB = await medico.save();
+
+
+        res.json({
+            ok: true,
+            medico: medicoDB
+        });
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            message: 'Error inesperado'
+        })
+    }
+}
+
+const actualizarMedico = (req, res = response) => {
+
+    res.json({
+        ok: true,
+        message: 'Medico actualizado'
+    })
+}
+
+const borrarMedico = (req, res = response) => {
+
+    res.json({
+        ok: true,
+        message: 'Medico borrado'
+    })
+}
+
+
+
+
+
+
+module.exports = {
+    getMedicos,
+    crearMedico,
+    actualizarMedico,
+    borrarMedico
+}
